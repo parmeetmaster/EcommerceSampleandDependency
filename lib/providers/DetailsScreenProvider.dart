9cd3b4f30eaaef15b35807463cd5e04656d9bbdd
parmeets834 <parmeets834@gmail.com>
2021-02-11 18:16:38 +0530

    description added

--- lib/providers/DetailsScreenProvider.dart
+++ lib/providers/DetailsScreenProvider.dart
@@ -1,60 +1,106 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:model_architecture/screens/DetailScreen/Components/curverdBox.dart';
 import 'package:model_architecture/screens/DetailScreen/Components/roundedCircle.dart';
 
 class DetailScreenProvider extends ChangeNotifier{
 
   int activeIndex=0;
   List<Color> productColors=[Colors.red,Colors.green,Colors.amber,Colors.pinkAccent];
-  List<Widget> data;
+  List<Widget> colorWidgets;
+  List<String> productSizes=["S","M","L","XL"];
+  List<Widget> product_size_list;
 
-  var price="\$80";
+  var price="\$ 80";
 
   generateColorCircles()async {
-    data=[];
+    colorWidgets=[];
     int i=0;
     for(Color item in productColors){
       if(i==0) {
-        data.add(InkWell(
+        colorWidgets.add(InkWell(
             onTap: (){
             changeActiveColor(item);
             },
             child: RoundedCircle( color: item, isborderActive: true)));
       }else {
-        data.add(InkWell(
+        colorWidgets.add(InkWell(
             onTap: (){activeIndex=i;
             changeActiveColor(item);
             },
             child: RoundedCircle( color: item, isborderActive: false)));
       }
-      data.add( SizedBox(height: 10,));
+      colorWidgets.add( SizedBox(height: 10,));
       print("$i");
       i++;
     }
   }
 
   changeActiveColor(Color activecolor){
-    data=[];
+    colorWidgets=[];
     int i=0;
     for(Color item in productColors){
       if(item==activecolor) {
-        data.add(InkWell(
+        colorWidgets.add(InkWell(
             onTap: (){
               changeActiveColor(item);
             },
             child: RoundedCircle( color: item, isborderActive: true)));
       }else {
-        data.add(InkWell(
+        colorWidgets.add(InkWell(
             onTap: (){changeActiveColor(item);},
             child: RoundedCircle(color: item, isborderActive: false)));
       }
-      data.add( SizedBox(height: 10,));
+      colorWidgets.add( SizedBox(height: 10,));
    i++;
     }
    notifyListeners();
   }
 
+generateCurveBoxes()async{
+  product_size_list=[];
+  int i=0;
+  for(String text in productSizes){
+    if(i==0) {
+      product_size_list.add(InkWell(
+          onTap: (){
+            changeActiveBox(text);
+          },
+          child: CurvedBox( text:text, isActive: true,)));
+    }else {
+      product_size_list.add(InkWell(
+          onTap: (){
+            changeActiveBox(text);
+          },
+          child: CurvedBox( text:text, isActive: false,)));
+    }
+    product_size_list.add( SizedBox(height: 10,));
+    print("$i");
+    i++;
+  }
 
+}
 
+changeActiveBox(String item){
+    print("change active box $item");
+  product_size_list=[];
+  int i=0;
+  for(String x in productSizes){
+    if(x==item) {
+      product_size_list.add(InkWell(
+          onTap: (){
+            changeActiveBox(x);
+          },
+          child: CurvedBox(text:x, isActive: true,)));
+    }else {
+      product_size_list.add(InkWell(
+          onTap: (){changeActiveBox(x);},
+          child: CurvedBox( text:x, isActive: false,)));
+    }
+    product_size_list.add( SizedBox(height: 10,));
+    i++;
+  }
+  notifyListeners();
+}
 }
